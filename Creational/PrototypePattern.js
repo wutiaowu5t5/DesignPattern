ab2236d9ac47770a487425a82d0abb4d4d9e610c
wutiaowu5t5 <1987053649@qq.com>
2024-04-09 18:31:45 +0800

    docs：Complete the use of prototype mode and singleton mode

--- Creational/PrototypePattern.js
+++ Creational/PrototypePattern.js
@@ -35,16 +35,17 @@ class GenericPrototypePattern {
 
 export default GenericPrototypePattern
 
+
 // region HOW TO USE
 // 使用原型模式创建新对象
-//const obj1 = new GenericPrototypePattern({name: 'Object 1', value: 10});
-//const obj2 = obj1.clone();
+const obj1 = new GenericPrototypePattern({name: 'Object 1', value: 10});
+const obj2 = obj1.clone();
 
 // 修改克隆后的对象的属性
-//obj2.name = 'Object 2';
-//obj2.color = 'blue';
+obj2.name = 'Object 2';
+obj2.color = 'blue';
 
 // 调用通用方法
-//obj1.printProperties(); // 输出: name: Object 1, value: 10
-//obj2.printProperties(); // 输出: name: Object 2, value: 10, color: blue
+obj1.printProperties(); // 输出: name: Object 1, value: 10
+obj2.printProperties(); // 输出: name: Object 2, value: 10, color: blue
 // endregion HOW TO USE
