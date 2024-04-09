dd52e4ee7592afb1fcae1e6dc76e376adfa11309
wutiaowu5t5 <1987053649@qq.com>
2024-04-09 18:40:36 +0800

    refactor：improved prototype mode

--- Creational/PrototypePattern.js
+++ Creational/PrototypePattern.js
@@ -6,30 +6,18 @@
 
 class GenericPrototypePattern {
     constructor(properties) {
-        for (let prop in properties) {
-            if (properties.hasOwnProperty(prop)) {
-                this[prop] = properties[prop];
-            }
-        }
+        Object.assign(this, properties) // 使用 Object.assign 进行属性复制
     }
     
     printProperties() {
-        for (let prop in this) {
-            if (this.hasOwnProperty(prop)) {
-                console.log(`${prop}: ${this[prop]}`);
-            }
+        for (let prop of Object.keys(this)) { // 使用 Object.keys 获取所有可枚举属性
+            console.log(`${prop}: ${this[prop]}`)
         }
     }
     
     clone() {
-        // 创建一个新的对象，将当前对象的属性复制到新对象上
-        let clonedObject = new GenericPrototypePattern({});
-        for (let prop in this) {
-            if (this.hasOwnProperty(prop)) {
-                clonedObject[prop] = this[prop];
-            }
-        }
-        return clonedObject;
+        // 创建一个新对象并进行浅拷贝
+        return new GenericPrototypePattern({...this})
     }
 }
 
@@ -38,14 +26,14 @@ export default GenericPrototypePattern
 
 // region HOW TO USE
 // 使用原型模式创建新对象
-const obj1 = new GenericPrototypePattern({name: 'Object 1', value: 10});
-const obj2 = obj1.clone();
+const obj1 = new GenericPrototypePattern({name: 'Object 1', value: 10})
+const obj2 = obj1.clone()
 
 // 修改克隆后的对象的属性
-obj2.name = 'Object 2';
-obj2.color = 'blue';
+obj2.name = 'Object 2'
+obj2.color = 'blue'
 
 // 调用通用方法
-obj1.printProperties(); // 输出: name: Object 1, value: 10
-obj2.printProperties(); // 输出: name: Object 2, value: 10, color: blue
+obj1.printProperties() // 输出: name: Object 1, value: 10
+obj2.printProperties() // 输出: name: Object 2, value: 10, color: blue
 // endregion HOW TO USE
