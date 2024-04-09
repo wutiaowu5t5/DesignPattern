626fddf758159d2667c24d46fce5089152711f33
wutiaowu5t5 <1987053649@qq.com>
2024-04-09 18:25:57 +0800

    docs：Complete the use of prototype mode and singleton mode

--- Creational/SingletonPattern.js
+++ Creational/SingletonPattern.js
@@ -35,3 +35,19 @@ class GenericSingletonPattern {
     }
     
 }
+
+
+export default GenericSingletonPattern
+
+// region HOW TO USE
+// 获取单例对象的实例
+//const instance1 = GenericSingletonPattern.getInstance();
+//const instance2 = GenericSingletonPattern.getInstance();
+
+// 检查是否是同一个实例
+//console.log(instance1 === instance2); // 输出: true
+
+// 试图使用 new 关键字创建实例，会抛出错误
+// const instance3 = new GenericSingletonPattern(); // 抛出错误: Cannot instantiate singleton class using new operator.
+// endregion HOW TO USE
+
