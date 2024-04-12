1cd54214b63a042a3138fccb32e86aa2f4c91afc
wutiaowu5t5 <1987053649@qq.com>
2024-04-12 16:12:23 +0800

    feat：add use AdapterPattern

---
+++ StructuralType/AdapterPattern/UseAdapterPattern/UseAdapterPattern.js
@@ -0,0 +1,30 @@
+/*
+ * @Description: 怎么使用适配器模式
+ * @Author: 5t5
+ * @Time: 2024/4/12 16:11
+ */
+
+// 使用ES6类语法定义旧的数据库接口
+class OldDatabase {
+    operate() {
+        // 旧的数据库操作
+        console.log('Operation on old database')
+    }
+}
+
+// 使用ES6类语法定义新的数据库接口
+class NewDatabase {
+    request() {
+        // 新的数据库操作
+        console.log('Operation on new database')
+    }
+}
+
+// 使用旧的数据库接口
+const oldDB = new OldDatabase()
+oldDB.operate() // 输出: Operation on old database
+
+// 使用适配器与新的数据库接口
+const newDB = new NewDatabase()
+const adaptedDB = new DatabaseAdapter(newDB)
+adaptedDB.operate() // 输出: Operation on new database
