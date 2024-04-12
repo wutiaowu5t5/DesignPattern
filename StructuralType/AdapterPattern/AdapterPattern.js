0fa48507d8721d8f66cdb49d5b5407e5dd34f6c1
wutiaowu5t5 <1987053649@qq.com>
2024-04-12 16:11:59 +0800

    feat：add AdapterPattern

---
+++ StructuralType/AdapterPattern/AdapterPattern.js
@@ -0,0 +1,17 @@
+/*
+ * @Description: 结构型-适配器模式
+ * @Author: 5t5
+ * @Time: 2024/4/12 16:10
+ */
+
+
+// 使用ES6类语法定义适配器
+class DatabaseAdapter {
+    constructor(newDatabase) {
+        this.database = newDatabase
+    }
+    
+    operate() {
+        this.database.request()
+    }
+}
