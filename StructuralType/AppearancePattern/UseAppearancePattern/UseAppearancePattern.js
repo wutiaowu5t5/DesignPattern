ef733e72f1ca30e4a35281c74c29d0da895d526a
wutiaowu5t5 <1987053649@qq.com>
2024-04-23 15:49:07 +0800

    docs：Complete the use appearance pattern

--- StructuralType/AppearancePattern/UseAppearancePattern/UseAppearancePattern.js
+++ StructuralType/AppearancePattern/UseAppearancePattern/UseAppearancePattern.js
@@ -6,11 +6,20 @@
 
 
 // 使用外观模式控制音频播放
+// 导入AudioPlayerFacade类
 import AudioPlayerFacade from '../AppearancePattern'
 
+// 创建一个AudioPlayerFacade的实例
 const audioPlayerFacade = new AudioPlayerFacade()
 
+// 播放音频文件"music.mp3"
 audioPlayerFacade.playAudio("music.mp3")
+
+// 暂停当前正在播放的音频
 audioPlayerFacade.pauseAudio()
+
+// 设置音量为80%
 audioPlayerFacade.setVolume(80)
+
+// 停止当前正在播放的音频
 audioPlayerFacade.stopAudio()
