0ff746aaf9c042af0300a535e0c3576b43c28324
wutiaowu5t5 <1987053649@qq.com>
2024-04-23 15:48:37 +0800

    feat：Complete the use appearance pattern

--- StructuralType/AppearancePattern/UseAppearancePattern/UseAppearancePattern.js
+++ StructuralType/AppearancePattern/UseAppearancePattern/UseAppearancePattern.js
@@ -3,3 +3,14 @@
  * @Author: 5t5
  * @Time: 2024/4/23 15:32
  */
+
+
+// 使用外观模式控制音频播放
+import AudioPlayerFacade from '../AppearancePattern'
+
+const audioPlayerFacade = new AudioPlayerFacade()
+
+audioPlayerFacade.playAudio("music.mp3")
+audioPlayerFacade.pauseAudio()
+audioPlayerFacade.setVolume(80)
+audioPlayerFacade.stopAudio()
