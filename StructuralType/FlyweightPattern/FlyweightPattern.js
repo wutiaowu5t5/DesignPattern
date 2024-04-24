a2a4917ceb55b6392cc0ace82387110be2320d1d
wutiaowu5t5 <1987053649@qq.com>
2024-04-24 16:26:31 +0800

    feat：Complete the flyweight pattern

--- StructuralType/FlyweightPattern/FlyweightPattern.js
+++ StructuralType/FlyweightPattern/FlyweightPattern.js
@@ -1,5 +1,39 @@
 /*
- * @Description: FlyweightPattern.js
+ * @Description: 结构型-享元模式
  * @Author: 5t5
  * @Time: 2024/4/24 16:05
  */
+
+
+class Character {
+    constructor(char) {
+        if (typeof char !== 'string' || char.length !== 1) {
+            throw new Error('Character must be a single character string.')
+        }
+        this.char = char
+    }
+    
+    display(position) {
+        if (!Array.isArray(position) || position.length !== 2) {
+            throw new Error('Position must be an array of two numbers.')
+        }
+        console.log(`Character '${this.char}' displayed at position (${position[0]}, ${position[1]})`)
+    }
+}
+
+class CharacterFactory {
+    #characters = {} // 私有属性
+    
+    getCharacter(char) {
+        if (typeof char !== 'string' || char.length !== 1) {
+            throw new Error('Character must be a single character string.')
+        }
+        
+        if (!(char in this.#characters)) {
+            this.#characters[char] = new Character(char)
+        }
+        return this.#characters[char]
+    }
+}
+
+export default new CharacterFactory()
