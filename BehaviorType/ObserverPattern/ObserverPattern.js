8d384fd3b7f0693d7fde0d1fb9643f1aada5dc17
wutiaowu5t5 <1987053649@qq.com>
2024-04-24 16:02:36 +0800

    feat：Complete the observer pattern

--- BehaviorType/ObserverPattern/ObserverPattern.js
+++ BehaviorType/ObserverPattern/ObserverPattern.js
@@ -3,3 +3,31 @@
  * @Author: 5t5
  * @Time: 2024/4/24 15:53
  */
+// Observer.js
+class Observer {
+    constructor() {
+        this.observers = new Set()
+    }
+    
+    subscribe(fn) {
+        if (typeof fn !== 'function') {
+            throw new Error('Observer.subscribe: Expected function')
+        }
+        this.observers.add(fn)
+    }
+    
+    unsubscribe(fn) {
+        if (typeof fn !== 'function') {
+            throw new Error('Observer.unsubscribe: Expected function')
+        }
+        this.observers.delete(fn)
+    }
+    
+    notify(data) {
+        this.observers.forEach(observer => observer(data))
+    }
+}
+
+const Watcher = Object.freeze(new Observer())
+
+export default Watcher
