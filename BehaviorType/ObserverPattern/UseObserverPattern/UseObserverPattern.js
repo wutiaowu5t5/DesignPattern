1ec7870f6b323ea990915374051f1c30d878a05e
wutiaowu5t5 <1987053649@qq.com>
2024-04-24 16:02:29 +0800

    feat：Complete the use observer pattern

--- BehaviorType/ObserverPattern/UseObserverPattern/UseObserverPattern.js
+++ BehaviorType/ObserverPattern/UseObserverPattern/UseObserverPattern.js
@@ -3,3 +3,18 @@
  * @Author: 5t5
  * @Time: 2024/4/24 15:53
  */
+
+
+import Watcher from '../ObserverPattern'
+
+const observer1 = data => console.log(`Observer 1 received: ${data}`)
+const observer2 = data => console.log(`Observer 2 received: ${data}`)
+
+Watcher.subscribe(observer1)
+Watcher.subscribe(observer2)
+
+Watcher.notify('Hello World')
+
+Watcher.unsubscribe(observer1)
+
+Watcher.notify('How are you?')
