3ae40b8e175dbad85490600b77ed9ceadf235273
wutiaowu5t5 <1987053649@qq.com>
2024-04-29 18:13:59 +0800

    refactor：complete the credit card strategy

--- BehaviorType/StrategyPattern/SubClass/Expansion/CreditCardStrategy.js
+++ BehaviorType/StrategyPattern/SubClass/Expansion/CreditCardStrategy.js
@@ -7,15 +7,29 @@
 import BasicPaymentStrategy from '../Basic/BasicPaymentStrategy'
 
 class CreditCardStrategy extends BasicPaymentStrategy {
-    constructor(cardNumber, cvv, expirationDate) {
+    constructor() {
         super()
-        const _cardNumber = cardNumber
-        const _cvv = cvv
-        const _expirationDate = expirationDate
-
-        this.pay = amount => {
-            console.log(`使用信用卡支付，卡号: ****${_cardNumber.slice(-4)}, CVV: ${_cvv}, 金额: ${amount}`)
+        this._cardNumber = null
+        this._cvv = null
+        this._expirationDate = null
+        
+        // 将pay方法定义在原型上，以便所有实例共享，同时便于在调用前检查状态
+    }
+    
+    setCardDetails(cardNumber, cvv, expirationDate) {
+        if (!cardNumber || !cvv || !expirationDate) {
+            throw new Error('cardNumber, cvv, 和 expirationDate 都是必填项')
         }
+        this._cardNumber = cardNumber
+        this._cvv = cvv
+        this._expirationDate = expirationDate
+    }
+    
+    pay(amount) {
+        if (!this._cardNumber || !this._cvv || !this._expirationDate) {
+            throw new Error('请先调用 setCardDetails 设置卡信息')
+        }
+        console.log(`使用信用卡支付，卡号: ****${this._cardNumber.slice(-4)}, CVV: ${this._cvv}, 金额: ${amount}`)
     }
 }
 
