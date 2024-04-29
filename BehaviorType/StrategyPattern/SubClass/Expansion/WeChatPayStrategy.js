9e4a8e5d149e08e2a7280ba288eb364fb2ac5333
wutiaowu5t5 <1987053649@qq.com>
2024-04-29 17:56:05 +0800

    feat：complete the weChat pay strategy

--- BehaviorType/StrategyPattern/SubClass/Expansion/WeChatPayStrategy.js
+++ BehaviorType/StrategyPattern/SubClass/Expansion/WeChatPayStrategy.js
@@ -1,5 +1,21 @@
 /*
- * @Description: WeChatPayStrategy.js
+ * @Description: 具体支付策略-微信
  * @Author: 5t5
  * @Time: 2024/4/29 17:46
  */
+
+import { PaymentStrategy } from '../../StrategyPattern'
+
+class WeChatPayStrategy extends PaymentStrategy {
+    constructor(appId, apiKey) {
+        super()
+        const _appId = appId // 使用下划线前缀是一种常见的非正式私有属性表示法
+        const _apiKey = apiKey
+
+        this.pay = function(amount) {
+            console.log(`使用微信支付，AppId: ${_appId}, 金额: ${amount}`)
+        }
+    }
+}
+
+export default new WeChatPayStrategy
