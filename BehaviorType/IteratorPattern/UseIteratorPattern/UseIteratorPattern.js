5ae3cb0af849bc00d2d6a68faf3ba7b540240e7a
wutiaowu5t5 <1987053649@qq.com>
2024-04-29 17:13:55 +0800

    docs：Complete the use iterator pattern

--- BehaviorType/IteratorPattern/UseIteratorPattern/UseIteratorPattern.js
+++ BehaviorType/IteratorPattern/UseIteratorPattern/UseIteratorPattern.js
@@ -3,14 +3,21 @@
  * @Author: 5t5
  * @Time: 2024/4/25 10:07
  */
-// 使用示例
-const list = new ListCollection()
 
-// 添加不同类型的元素
-list.add(1);
-list.add(function() { console.log('Function executed!'); })
-list.add({ key: 'value' })
-list.add('Text')
+import ListCollection from '../IteratorPattern'
 
-// 调用forEachExecuteOrLog方法遍历并处理集合
-list.forEachExecuteOrLog()
+/**
+ * 向列表中添加各种类型的元素。
+ * 包括数字、函数、对象和字符串。
+ */
+ListCollection.add(1);
+ListCollection.add(function() { console.log('Function executed!') })
+ListCollection.add({ key: 'value' })
+ListCollection.add('Text')
+
+/**
+ * 遍历列表中的每个元素，并尝试执行它们。
+ * 如果元素是函数，则执行该函数；
+ * 如果元素是字符串或其他类型，则仅在控制台打印。
+ */
+ListCollection.forEachExecuteOrLog()
