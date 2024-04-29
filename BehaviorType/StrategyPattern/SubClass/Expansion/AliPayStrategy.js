e9731c7090f005db18a14d1f031d3f83ae715add
wutiaowu5t5 <1987053649@qq.com>
2024-04-29 18:13:30 +0800

    refactor：complete the ali pay strategy

--- BehaviorType/StrategyPattern/SubClass/Expansion/AliPayStrategy.js
+++ BehaviorType/StrategyPattern/SubClass/Expansion/AliPayStrategy.js
@@ -7,13 +7,26 @@
 import BasicPaymentStrategy from '../Basic/BasicPaymentStrategy'
 
 class AliPayStrategy extends BasicPaymentStrategy {
-    constructor(partnerId, sellerId) {
+    constructor() {
         super()
-        this._partnerId = partnerId // 前缀下划线是一种常见的私有属性模拟方式
+        this._partnerId = null // 初始化为null，表示未设置
+        this._sellerId = null // 同上
+    }
+    
+    // 新增方法用于设置参数
+    setCredentials(partnerId, sellerId) {
+        if (!partnerId || !sellerId) {
+            throw new Error('partnerId 和 sellerId 都是必填项')
+        }
+        this._partnerId = partnerId
         this._sellerId = sellerId
     }
-
+    
+    // 在调用核心功能前检查参数是否已设置
     pay(amount) {
+        if (!this._partnerId || !this._sellerId) {
+            throw new Error('请先调用 setCredentials 设置 PartnerId 和 SellerId')
+        }
         console.log(`使用支付宝支付，PartnerId: ${this._partnerId}, SellerId: ${this._sellerId}, 金额: ${amount}`)
     }
 }
