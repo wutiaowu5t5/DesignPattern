d042b5eb85181c3bc12048fb6407182f031473a1
wutiaowu5t5 <1987053649@qq.com>
2024-04-29 18:00:10 +0800

    feat：complete the basic payment strategy

--- BehaviorType/StrategyPattern/SubClass/Basic/BasicPaymentStrategy.js
+++ BehaviorType/StrategyPattern/SubClass/Basic/BasicPaymentStrategy.js
@@ -1,5 +1,13 @@
 /*
- * @Description: 支付策略接口
+ * @Description: 基础支付策略接口
  * @Author: 5t5
  * @Time: 2024/4/29 17:45
  */
+
+class BasicPaymentStrategy {
+    pay(amount) {
+        throw new Error("pay() 方法必须在子类中实现")
+    }
+}
+
+export default BasicPaymentStrategy
