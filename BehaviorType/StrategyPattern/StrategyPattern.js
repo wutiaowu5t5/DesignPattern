e8059a0374b80a96b9d95f8808acc7a852bedd01
wutiaowu5t5 <1987053649@qq.com>
2024-04-29 18:32:29 +0800

    docs：complete the strategy pattern

--- BehaviorType/StrategyPattern/StrategyPattern.js
+++ BehaviorType/StrategyPattern/StrategyPattern.js
@@ -12,14 +12,14 @@ class PaymentStrategyFactory {
      * 构造函数，初始化一个空的策略映射
      */
     constructor() {
-        this.strategies = new Map();
+        this.strategies = new Map()
     }
     
     /**
      * 注册一个新的支付策略
      * @param {string} name 策略的名称
      * @param {PaymentStrategy} strategy 实现了PaymentStrategy接口的策略实例
-     * @throws 如果策略没有实现PaymentStrategy接口，则抛出错误
+     * @throws {Error} 如果策略没有实现PaymentStrategy接口，则抛出错误
      */
     register(name, strategy) {
         if (!(strategy instanceof PaymentStrategy)) {
@@ -31,7 +31,7 @@ class PaymentStrategyFactory {
     /**
      * 注销一个已注册的支付策略
      * @param {string} name 要注销的策略名称
-     * @returns 如果策略被成功注销，则无返回值；如果策略不存在，则打印警告
+     * @returns {Error} 如果策略被成功注销，则无返回值；如果策略不存在，则打印警告
      */
     unregister(name) {
         if (this.strategies.has(name)) {
@@ -45,7 +45,7 @@ class PaymentStrategyFactory {
      * 获取一个指定名称的支付策略
      * @param {string} name 策略的名称
      * @return {PaymentStrategy} 返回指定名称的支付策略实例
-     * @throws 如果未找到指定名称的策略，则抛出错误
+     * @throws {Error} 如果未找到指定名称的策略，则抛出错误
      */
     getStrategy(name) {
         const strategy = this.strategies.get(name)
@@ -72,7 +72,7 @@ class Checkout {
      * 处理支付请求
      * @param {number} amount 需要支付的金额
      * @param {string} strategyName 支付策略的名称
-     * @throws 如果找不到指定的支付策略，则抛出错误
+     * @throws {Error} 如果找不到指定的支付策略，则抛出错误
      */
     processPayment(amount, strategyName) {
         const strategy = this.paymentFactory.getStrategy(strategyName)
