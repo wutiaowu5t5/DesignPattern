697786a768df1c7b9bab5d97602b6095d79e37ef
wutiaowu5t5 <1987053649@qq.com>
2024-04-29 17:52:01 +0800

    feat：complete the credit card pay strategy

--- BehaviorType/StrategyPattern/SubClass/Expansion/CreditCardStrategy.js
+++ BehaviorType/StrategyPattern/SubClass/Expansion/CreditCardStrategy.js
@@ -1,5 +1,23 @@
 /*
- * @Description: CreditCardStrategy.js
+ * @Description: 具体支付策略-信用卡
  * @Author: 5t5
  * @Time: 2024/4/29 17:47
  */
+
+import { PaymentStrategy } from '../../StrategyPattern'
+
+class CreditCardStrategy extends PaymentStrategy {
+    constructor(cardNumber, cvv, expirationDate) {
+        super()
+        const _cardNumber = cardNumber
+        const _cvv = cvv
+        const _expirationDate = expirationDate
+
+        this.pay = amount => {
+            console.log(`使用信用卡支付，卡号: ****${_cardNumber.slice(-4)}, CVV: ${_cvv}, 金额: ${amount}`)
+        }
+    }
+}
+
+
+export default new CreditCardStrategy
