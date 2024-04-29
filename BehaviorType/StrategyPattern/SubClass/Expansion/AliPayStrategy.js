f93a196da30c8d076d679d4c95c4f3920877d0a6
wutiaowu5t5 <1987053649@qq.com>
2024-04-29 17:54:52 +0800

    feat：complete the ali pay strategy

--- BehaviorType/StrategyPattern/SubClass/Expansion/AliPayStrategy.js
+++ BehaviorType/StrategyPattern/SubClass/Expansion/AliPayStrategy.js
@@ -1,5 +1,21 @@
 /*
- * @Description: AliPayStrategy.js
+ * @Description: 具体支付策略-支付宝
  * @Author: 5t5
  * @Time: 2024/4/29 17:47
  */
+
+import { PaymentStrategy } from '../../StrategyPattern'
+
+class AliPayStrategy extends PaymentStrategy {
+    constructor(partnerId, sellerId) {
+        super()
+        this._partnerId = partnerId // 前缀下划线是一种常见的私有属性模拟方式
+        this._sellerId = sellerId
+    }
+
+    pay(amount) {
+        console.log(`使用支付宝支付，PartnerId: ${this._partnerId}, SellerId: ${this._sellerId}, 金额: ${amount}`)
+    }
+}
+
+export default new AliPayStrategy
