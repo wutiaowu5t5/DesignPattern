814bf73495ef8c50b2fd71ae8a98f3df902f5ea8
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 11:24:17 +0800

    feat：complete the subclass of chain of responsibility pattern

--- BehaviorType/ChainOfResponsibilityPattern/SubClass/TeamLeader.js
+++ BehaviorType/ChainOfResponsibilityPattern/SubClass/TeamLeader.js
@@ -1,5 +1,27 @@
 /*
- * @Description: TeamLeader.js
+ * @Description: 职责链模式子类-TeamLeader
  * @Author: 5t5
  * @Time: 2024/5/8 11:19
  */
+
+import Processor from '../ChainOfResponsibilityPattern'
+
+class TeamLeader extends Processor {
+    constructor() {
+        super("团队领导", 2)
+    }
+    
+    // 实现抽象方法
+    processRequest(request) {
+        if (request.leaveDays <= this.authorityLevel) {
+            console.log(`${this.name}审批了${request.leaveDays}天的请假。`)
+        } else if (this.nextProcessor) {
+            console.log(`${this.name}无法处理，请转交给${this.nextProcessor.name}`)
+            this.nextProcessor.processRequest(request)
+        } else {
+            console.log(`无法处理，请假天数过长。`)
+        }
+    }
+}
+
+export default TeamLeader
