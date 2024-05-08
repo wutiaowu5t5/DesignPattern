40ab578d6d3dfb23d5e31e7af0ade45760cde656
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 11:03:51 +0800

    docs：complete the subclass of template method pattern

--- BehaviorType/TemplateMethodPattern/SubClass/Tea.js
+++ BehaviorType/TemplateMethodPattern/SubClass/Tea.js
@@ -4,13 +4,24 @@
  * @Time: 2024/5/8 10:53
  */
 
+/**
+ * Tea 类继承自 Beverage 类，实现了冲泡茶的具体步骤。
+ */
 import Beverage from '../TemplateMethodPattern'
 
 class Tea extends Beverage {
+    /**
+     * 冲泡茶叶的方法。
+     * 此方法为 Tea 类实现的抽象方法，具体冲泡过程为：冲泡茶叶。
+     */
     brew() {
         console.log('冲泡茶叶')
     }
     
+    /**
+     * 添加调料的方法。
+     * 对于茶来说，通常不添加额外的调料，因此此方法实现为不进行任何操作。
+     */
     addCondiments() {
         console.log('不加东西')
     }
