86e6d86663b3d117c7a4d4c81a3877b672bc4a2c
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 16:50:00 +0800

    docs：complete the state pattern

--- BehaviorType/StatePattern/StatePattern.js
+++ BehaviorType/StatePattern/StatePattern.js
@@ -4,17 +4,28 @@
  * @Time: 2024/5/7 18:20
  */
 
+/**
+ * 自动售货机类，管理产品状态和行为。
+ */
 import VendingMachineState from './SubClass/VendingMachineState'
 import HasProductState from './SubClass/HasProductState'
 import SoldOutState from './SubClass/SoldOutState'
 
 class VendingMachine {
+    /**
+     * 构造函数初始化自动售货机状态。
+     * @param {number} initialStock 初始库存量。
+     */
     constructor(initialStock) {
-        this.hasProductState = new HasProductState(this)
-        this.soldOutState = new SoldOutState(this)
-        this.currentState = initialStock > 0 ? this.hasProductState : this.soldOutState
+        this.hasProductState = new HasProductState(this) // 当有产品时的状态
+        this.soldOutState = new SoldOutState(this) // 当产品售罄时的状态
+        this.currentState = initialStock > 0 ? this.hasProductState : this.soldOutState // 根据初始库存设置当前状态
     }
     
+    /**
+     * 设置自动售货机的状态。
+     * @param {VendingMachineState} newState 新的状态对象。
+     */
     setState(newState) {
         if (newState instanceof VendingMachineState) {
             console.log(`状态变化：从${this.currentState.constructor.name}到${newState.constructor.name}`)
@@ -24,18 +35,32 @@ class VendingMachine {
         }
     }
     
+    /**
+     * 获取有产品状态对象。
+     * @returns {HasProductState} 有产品状态对象。
+     */
     getHasProductState() {
         return this.hasProductState
     }
     
+    /**
+     * 获取售罄状态对象。
+     * @returns {SoldOutState} 售罄状态对象。
+     */
     getSoldOutState() {
         return this.soldOutState
     }
     
+    /**
+     * 销售产品，行为取决于当前状态。
+     */
     sellProduct() {
         this.currentState.sellProduct()
     }
     
+    /**
+     * 补充库存，行为取决于当前状态。
+     */
     restock() {
         this.currentState.restock()
     }
