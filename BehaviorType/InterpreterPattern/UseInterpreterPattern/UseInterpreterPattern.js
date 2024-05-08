024a7153011fb58b18caac99a64f6df9102bf543
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 18:18:13 +0800

    feat：complete the use interpreter pattern

--- BehaviorType/InterpreterPattern/UseInterpreterPattern/UseInterpreterPattern.js
+++ BehaviorType/InterpreterPattern/UseInterpreterPattern/UseInterpreterPattern.js
@@ -1,5 +1,16 @@
 /*
- * @Description: UseInterpreterPattern.js
+ * @Description: 怎么使用解释器模式
  * @Author: 5t5
  * @Time: 2024/5/7 18:23
  */
+
+import { add, multiply, NumberExpression } from '../InterpreterPattern'
+
+const expression = add(
+    new NumberExpression(3),
+    multiply(
+        new NumberExpression(5),
+        new NumberExpression(2)
+    )
+)
+console.log(expression.interpret()) // Output: 13
