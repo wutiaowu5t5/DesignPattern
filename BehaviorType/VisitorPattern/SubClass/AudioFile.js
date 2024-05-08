7ff8db04b14dcf142dae2a6f1dbd94fd81278311
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 17:21:56 +0800

    feat：complete the subClass of visitor pattern

--- BehaviorType/VisitorPattern/SubClass/AudioFile.js
+++ BehaviorType/VisitorPattern/SubClass/AudioFile.js
@@ -3,3 +3,18 @@
  * @Author: 5t5
  * @Time: 2024/5/8 17:17
  */
+
+import FileSystem from './File'
+
+class AudioFile extends FileSystem {
+    constructor(name, audio) {
+        super(name)
+        this.audio = audio
+    }
+    
+    getType() {
+        return 'audio'
+    }
+}
+
+export default AudioFile
