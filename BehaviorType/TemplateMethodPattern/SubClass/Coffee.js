7bc5344942063ecb462c42e4887db1e1958055bd
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 11:02:05 +0800

    feat：complete the subClass of template method pattern

--- BehaviorType/TemplateMethodPattern/SubClass/Coffee.js
+++ BehaviorType/TemplateMethodPattern/SubClass/Coffee.js
@@ -1,5 +1,19 @@
 /*
- * @Description: Coffee.js
+ * @Description: 模板方法模式子类-咖啡
  * @Author: 5t5
  * @Time: 2024/5/8 10:53
  */
+
+import Beverage from '../TemplateMethodPattern'
+
+class Coffee extends Beverage {
+    brew() {
+        console.log('冲泡咖啡')
+    }
+    
+    addCondiments() {
+        console.log('加糖和牛奶')
+    }
+}
+
+export default Coffee
