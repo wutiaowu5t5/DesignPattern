79cd7c99735188daccdaf62caa20f18ac602ba2b
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 17:21:59 +0800

    feat：complete the subClass of visitor pattern

--- BehaviorType/VisitorPattern/SubClass/File.js
+++ BehaviorType/VisitorPattern/SubClass/File.js
@@ -3,3 +3,29 @@
  * @Author: 5t5
  * @Time: 2024/5/8 17:16
  */
+
+import { Visitor } from '../VisitorPattern'
+
+class FileSystem {
+    constructor(name) {
+        if (new.target === FileSystem) {
+            throw new TypeError('Cannot construct File instances directly')
+        }
+        this.name = name
+    }
+    
+    // 接受访问者对象
+    accept(visitor) {
+        if (!(visitor instanceof Visitor)) {
+            throw new TypeError('Visitor object expected')
+        }
+        visitor.visit(this)
+    }
+    
+    // 获取文件类型的方法
+    getType() {
+        throw new Error("getType method must be implemented by subclasses")
+    }
+}
+
+export default FileSystem
