bf45f18f968f8aff8db7f8a26193bb8d3e50580c
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 14:58:00 +0800

    feat：complete the use command pattern

--- BehaviorType/CommandPattern/UseCommandPattern/UseCommandPattern.js
+++ BehaviorType/CommandPattern/UseCommandPattern/UseCommandPattern.js
@@ -3,3 +3,36 @@
  * @Author: 5t5
  * @Time: 2024/5/7 18:17
  */
+
+import RemoteControl from '../CommandPattern'
+import Television from '../SubClass/Television'
+import Stereo from '../SubClass/Stereo'
+import { TurnOffCommand, TurnOnCommand } from '../SubClass/Command'
+
+const tv = new Television()
+const stereo = new Stereo()
+
+const turnOnTvCommand = new TurnOnCommand(tv)
+const turnOffTvCommand = new TurnOffCommand(tv)
+
+const turnOnStereoCommand = new TurnOnCommand(stereo)
+const turnOffStereoCommand = new TurnOffCommand(stereo)
+
+const remoteControl = new RemoteControl()
+
+remoteControl.setCommand(turnOnTvCommand)
+remoteControl.setCommand(turnOffTvCommand)
+remoteControl.setCommand(turnOnStereoCommand)
+remoteControl.setCommand(turnOffStereoCommand)
+
+remoteControl.pressButton() // 输出：电视已打开
+remoteControl.pressButton() // 输出：电视已关闭
+remoteControl.pressButton() // 输出：音响已打开
+remoteControl.pressButton() // 输出：音响已关闭
+
+remoteControl.pressUndo()   // 输出：音响已打开
+remoteControl.pressUndo()   // 输出：音响已关闭
+remoteControl.pressUndo()   // 输出：电视已打开
+remoteControl.pressUndo()   // 输出：电视已关闭
+
+remoteControl.pressUndo()   // 输出：没有可撤销的命令
