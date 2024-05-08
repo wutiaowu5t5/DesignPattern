5fcd5da90dafe3f9762e6c91d424ea804e930dc8
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 14:57:52 +0800

    feat：complete the subclass of command pattern

--- BehaviorType/CommandPattern/SubClass/Command.js
+++ BehaviorType/CommandPattern/SubClass/Command.js
@@ -3,3 +3,46 @@
  * @Author: 5t5
  * @Time: 2024/5/8 14:50
  */
+
+class Command {
+    constructor(device) {
+        if (new.target === Command) {
+            throw new Error("本类不能实例化")
+        }
+        this.device = device
+    }
+    
+    execute() {
+        throw new Error("execute 方法必须被重写")
+    }
+    
+    undo() {
+        throw new Error("undo 方法必须被重写")
+    }
+}
+
+class TurnOnCommand extends Command {
+    execute() {
+        this.device.turnOn()
+    }
+    
+    undo() {
+        this.device.turnOff()
+    }
+}
+
+class TurnOffCommand extends Command {
+    execute() {
+        this.device.turnOff()
+    }
+    
+    undo() {
+        this.device.turnOn()
+    }
+}
+
+export {
+    Command,
+    TurnOnCommand,
+    TurnOffCommand
+}
