2d3543c7ad9e23ea0ae95a598f2938292f5d3904
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 11:02:21 +0800

    feat：complete the template method pattern

--- BehaviorType/TemplateMethodPattern/TemplateMethodPattern.js
+++ BehaviorType/TemplateMethodPattern/TemplateMethodPattern.js
@@ -1,5 +1,33 @@
 /*
- * @Description: TemplateMethodPattern.js
+ * @Description: 行为型-模板方法模式
  * @Author: 5t5
  * @Time: 2024/5/7 18:14
  */
+
+
+class Beverage {
+    prepareBeverage() {
+        this.boilWater()
+        this.brew()
+        this.pourInCup()
+        this.addCondiments()
+    }
+    
+    boilWater() {
+        console.log('煮水')
+    }
+    
+    pourInCup() {
+        console.log('倒入杯子')
+    }
+    
+    brew() {
+        throw new Error('子类必须实现brew方法')
+    }
+    
+    addCondiments() {
+        throw new Error('子类必须实现addCondiments方法')
+    }
+}
+
+export default Beverage
