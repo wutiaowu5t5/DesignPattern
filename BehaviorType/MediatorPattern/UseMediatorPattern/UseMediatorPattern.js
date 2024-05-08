a2329306b48e373ffc9fdc72d34827a856155eee
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 15:21:57 +0800

    feat：complete the use mediator pattern

--- BehaviorType/MediatorPattern/UseMediatorPattern/UseMediatorPattern.js
+++ BehaviorType/MediatorPattern/UseMediatorPattern/UseMediatorPattern.js
@@ -1,5 +1,26 @@
 /*
- * @Description: UseMediatorPattern.js
+ * @Description: 怎么使用备忘录模式
  * @Author: 5t5
  * @Time: 2024/5/7 18:23
  */
+
+import TextEditor from '../MediatorPattern'
+
+const editor = new TextEditor()
+
+editor.type('Hello, ')
+editor.save()
+editor.type('world!')
+editor.save()
+
+console.log(editor.getContent()) // 输出：Hello, world!
+
+editor.undo()
+console.log(editor.getContent()) // 输出：Hello,
+
+editor.undo()
+console.log(editor.getContent()) // 输出：（空字符串）
+
+editor.restore(1) // 输出：无效的历史索引
+
+console.log(editor.getHistory())
