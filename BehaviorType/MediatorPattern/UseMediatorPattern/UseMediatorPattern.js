b5d336a25d7560a9d2cb593ae7605568b8467734
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 17:53:32 +0800

    feat：complete the use mediator pattern

--- BehaviorType/MediatorPattern/UseMediatorPattern/UseMediatorPattern.js
+++ BehaviorType/MediatorPattern/UseMediatorPattern/UseMediatorPattern.js
@@ -1,5 +1,20 @@
 /*
- * @Description: UseMementoPattern.js
+ * @Description: 怎么使用中介者模式
  * @Author: 5t5
  * @Time: 2024/5/8 17:01
  */
+
+import { ChatRoom, User } from '../MediatorPattern'
+
+const chatRoom = new ChatRoom()
+
+const user1 = new User(1, 'User 1')
+const user2 = new User(2, 'User 2')
+const user3 = new User(3, 'User 3')
+
+user1.joinChatRoom(chatRoom)
+user2.joinChatRoom(chatRoom)
+user3.joinChatRoom(chatRoom)
+
+user1.send('Hello, everyone!')
+user2.send('Hi, there!')
