60438cf7f2d29b4aee5e0ec3c20b6656a0641cb5
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 11:02:16 +0800

    feat：complete the use template method pattern

--- BehaviorType/TemplateMethodPattern/UseTemplateMethodPattern/UseTemplateMethodPattern.js
+++ BehaviorType/TemplateMethodPattern/UseTemplateMethodPattern/UseTemplateMethodPattern.js
@@ -1,5 +1,16 @@
 /*
- * @Description: UseTemplateMethodPattern.js
+ * @Description: 怎么使用模板方法模式
  * @Author: 5t5
  * @Time: 2024/5/7 18:14
  */
+
+import Coffee from '../SubClass/Coffee'
+import Tea from '../SubClass/Tea'
+
+console.log('制作咖啡：')
+const coffee = new Coffee()
+coffee.prepareBeverage()
+
+console.log('\n制作茶：')
+const tea = new Tea()
+tea.prepareBeverage()
