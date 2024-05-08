c2007dfa3262b7005c168f34ed3ee5e6ea1532e1
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 15:01:09 +0800

    docs：complete the subclass of command pattern

--- BehaviorType/CommandPattern/SubClass/Stereo.js
+++ BehaviorType/CommandPattern/SubClass/Stereo.js
@@ -4,14 +4,28 @@
  * @Time: 2024/5/8 14:47
  */
 
+/**
+ * Stereo类代表一个音响系统。
+ */
 class Stereo {
+    /**
+     * 打开音响。
+     * 无参数。
+     * 无返回值。
+     */
     turnOn() {
         console.log("音响已打开")
     }
     
+    /**
+     * 关闭音响。
+     * 无参数。
+     * 无返回值。
+     */
     turnOff() {
         console.log("音响已关闭")
     }
 }
 
+// 导出Stereo类作为默认模块
 export default Stereo
