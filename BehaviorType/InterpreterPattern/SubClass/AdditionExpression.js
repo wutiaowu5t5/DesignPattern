4f389f5a46702a76629dc633ba9a8c88b26e9276
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 18:17:49 +0800

    feat：complete the subClass of interpreter pattern

--- BehaviorType/InterpreterPattern/SubClass/AdditionExpression.js
+++ BehaviorType/InterpreterPattern/SubClass/AdditionExpression.js
@@ -1,5 +1,15 @@
 /*
- * @Description: AdditionExpression.js
+ * @Description: 表达式子类-加法
  * @Author: 5t5
  * @Time: 2024/5/8 18:10
  */
+
+import { CompoundExpression } from '../InterpreterPattern'
+
+class AdditionExpression extends CompoundExpression {
+    interpret() {
+        return this.expression1.interpret() + this.expression2.interpret()
+    }
+}
+
+export default AdditionExpression
