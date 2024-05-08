42cdb6c793a4aaa2b8d1566b00d11655281fd14f
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 16:46:13 +0800

    feat：complete the subClass of state pattern

--- BehaviorType/StatePattern/SubClass/SoldOutState.js
+++ BehaviorType/StatePattern/SubClass/SoldOutState.js
@@ -1,5 +1,20 @@
 /*
- * @Description: SoldOutState.js
+ * @Description: 状态模式子类-没货状态基类
  * @Author: 5t5
  * @Time: 2024/5/8 16:39
  */
+
+import VendingMachineState from './VendingMachineState'
+
+class SoldOutState extends VendingMachineState {
+    sellProduct() {
+        console.log("无法售出，商品已售罄")
+    }
+    
+    restock() {
+        console.log("补货成功")
+        this.vendingMachine.setState(this.vendingMachine.getHasProductState())
+    }
+}
+
+export default SoldOutState
