261e0abe09a315bd264e490fb7d479d86b26e9ce
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 17:22:05 +0800

    feat：complete the subClass of visitor pattern

--- BehaviorType/VisitorPattern/SubClass/TextFile.js
+++ BehaviorType/VisitorPattern/SubClass/TextFile.js
@@ -3,3 +3,17 @@
  * @Author: 5t5
  * @Time: 2024/5/8 17:16
  */
+import FileSystem from './File'
+
+class TextFile extends FileSystem {
+    constructor(name, content) {
+        super(name)
+        this.content = content
+    }
+    
+    getType() {
+        return 'text'
+    }
+}
+
+export default TextFile
