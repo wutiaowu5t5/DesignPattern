6c187b990713cc492faa8ef8e1fd6fe97f7f1c8e
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 17:26:13 +0800

    docs：complete the subClass of visitor pattern

--- BehaviorType/VisitorPattern/SubClass/TextFile.js
+++ BehaviorType/VisitorPattern/SubClass/TextFile.js
@@ -3,14 +3,27 @@
  * @Author: 5t5
  * @Time: 2024/5/8 17:16
  */
+
+/**
+ * TextFile类继承自FileSystem类，用于处理文本文件的相关操作。
+ */
 import FileSystem from './File'
 
+/**
+ * 创建一个TextFile实例。
+ * @param {string} name 文件名。
+ * @param {string} content 文件内容。
+ */
 class TextFile extends FileSystem {
     constructor(name, content) {
-        super(name)
-        this.content = content
+        super(name) // 调用父类的构造函数
+        this.content = content // 设置文件内容
     }
     
+    /**
+     * 获取文件类型。
+     * @returns {string} 返回文件类型，此处固定为'text'。
+     */
     getType() {
         return 'text'
     }
