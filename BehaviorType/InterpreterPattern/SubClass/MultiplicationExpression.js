6ce0aa32ec5f348be3ba3adf9d7b0da8cc7335c9
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 18:22:22 +0800

    docs：complete the subClass of interpreter pattern

--- BehaviorType/InterpreterPattern/SubClass/MultiplicationExpression.js
+++ BehaviorType/InterpreterPattern/SubClass/MultiplicationExpression.js
@@ -4,10 +4,19 @@
  * @Time: 2024/5/8 18:11
  */
 
+/**
+ * MultiplicationExpression 类继承自 CompoundExpression，
+ * 用于表示乘法运算的表达式。
+ */
 import { CompoundExpression } from '../InterpreterPattern'
 
 class MultiplicationExpression extends CompoundExpression {
+    /**
+     * 解释执行乘法运算。
+     * @returns {number} 返回两个表达式相乘的结果。
+     */
     interpret() {
+        // 执行两个子表达式的解释操作，并返回其乘积
         return this.expression1.interpret() * this.expression2.interpret()
     }
 }
