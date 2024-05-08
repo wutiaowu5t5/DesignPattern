f3e2da39b7d812468256b71a39002defdc1e0c8d
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 11:02:08 +0800

    feat：complete the subClass of template method pattern

--- BehaviorType/TemplateMethodPattern/SubClass/Tea.js
+++ BehaviorType/TemplateMethodPattern/SubClass/Tea.js
@@ -1,5 +1,19 @@
 /*
- * @Description: Tea.js
+ * @Description: 模板方法模式子类-茶
  * @Author: 5t5
  * @Time: 2024/5/8 10:53
  */
+
+import Beverage from '../TemplateMethodPattern'
+
+class Tea extends Beverage {
+    brew() {
+        console.log('冲泡茶叶')
+    }
+    
+    addCondiments() {
+        console.log('不加东西')
+    }
+}
+
+export default Tea
