43f690d871a007ce22e3ade9231f9b19458d8b82
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 17:22:02 +0800

    feat：complete the subClass of visitor pattern

--- BehaviorType/VisitorPattern/SubClass/ImageFile.js
+++ BehaviorType/VisitorPattern/SubClass/ImageFile.js
@@ -3,3 +3,18 @@
  * @Author: 5t5
  * @Time: 2024/5/8 17:16
  */
+
+import FileSystem from './File'
+
+class ImageFile extends FileSystem {
+    constructor(name, data) {
+        super(name)
+        this.data = data
+    }
+    
+    getType() {
+        return 'image'
+    }
+}
+
+export default ImageFile
