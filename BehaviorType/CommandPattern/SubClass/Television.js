5af9b37b88f1566e29ac3e6a55a94fedf5f1ca33
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 15:00:49 +0800

    docs：complete the subclass of command pattern

--- BehaviorType/CommandPattern/SubClass/Television.js
+++ BehaviorType/CommandPattern/SubClass/Television.js
@@ -4,14 +4,28 @@
  * @Time: 2024/5/8 14:47
  */
 
+/**
+ * 电视类
+ */
 class Television {
+    /**
+     * 打开电视
+     * @无参数
+     * @无返回值
+     */
     turnOn() {
         console.log("电视已打开")
     }
     
+    /**
+     * 关闭电视
+     * @无参数
+     * @无返回值
+     */
     turnOff() {
         console.log("电视已关闭")
     }
 }
 
+// 导出电视类作为默认模块
 export default Television
