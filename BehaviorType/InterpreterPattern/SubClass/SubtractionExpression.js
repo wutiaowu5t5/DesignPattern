6dcb36b1d501cfb71382ec975e31baa4ff33a7ae
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 18:18:06 +0800

    feat：complete the subClass of interpreter pattern

--- BehaviorType/InterpreterPattern/SubClass/SubtractionExpression.js
+++ BehaviorType/InterpreterPattern/SubClass/SubtractionExpression.js
@@ -1,5 +1,15 @@
 /*
- * @Description: SubtractionExpression.js
+ * @Description: 表达式子类-减法
  * @Author: 5t5
  * @Time: 2024/5/8 18:10
  */
+
+import { CompoundExpression } from '../InterpreterPattern'
+
+class SubtractionExpression extends CompoundExpression {
+    interpret() {
+        return this.expression1.interpret() - this.expression2.interpret()
+    }
+}
+
+export default SubtractionExpression
