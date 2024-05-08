df7c61d0e3dcaea30b2414ae4712baace267cb13
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 18:22:02 +0800

    docs：complete the use interpreter pattern

--- BehaviorType/InterpreterPattern/UseInterpreterPattern/UseInterpreterPattern.js
+++ BehaviorType/InterpreterPattern/UseInterpreterPattern/UseInterpreterPattern.js
@@ -4,8 +4,12 @@
  * @Time: 2024/5/7 18:23
  */
 
+/**
+ * 导入解释器模式中的加法、乘法函数及数字表达式类
+ */
 import { add, multiply, NumberExpression } from '../InterpreterPattern'
 
+// 创建一个表达式，该表达式表示 3 加上 5 乘以 2 的结果
 const expression = add(
     new NumberExpression(3),
     multiply(
@@ -13,4 +17,6 @@ const expression = add(
         new NumberExpression(2)
     )
 )
-console.log(expression.interpret()) // Output: 13
+
+// 输出表达式的解释结果
+console.log(expression.interpret()) // 输出: 13
