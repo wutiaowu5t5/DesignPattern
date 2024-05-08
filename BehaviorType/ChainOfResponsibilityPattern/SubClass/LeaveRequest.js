48a8d06e4c966c917515f59342abf9a3a7404d50
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 11:24:15 +0800

    feat：complete the subclass of chain of responsibility pattern

--- BehaviorType/ChainOfResponsibilityPattern/SubClass/LeaveRequest.js
+++ BehaviorType/ChainOfResponsibilityPattern/SubClass/LeaveRequest.js
@@ -1,5 +1,13 @@
 /*
- * @Description: LeaveRequest.js
+ * @Description: 职责链模式子类-LeaveRequest
  * @Author: 5t5
  * @Time: 2024/5/8 11:19
  */
+
+class LeaveRequest {
+    constructor(leaveDays) {
+        this.leaveDays = leaveDays
+    }
+}
+
+export default LeaveRequest
