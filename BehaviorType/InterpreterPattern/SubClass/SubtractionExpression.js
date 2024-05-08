6a2c2512c364635a8f833a14c62f6ff5f3ed1d80
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 18:22:19 +0800

    docs：complete the subClass of interpreter pattern

--- BehaviorType/InterpreterPattern/SubClass/SubtractionExpression.js
+++ BehaviorType/InterpreterPattern/SubClass/SubtractionExpression.js
@@ -4,10 +4,19 @@
  * @Time: 2024/5/8 18:10
  */
 
+/**
+ * SubtractionExpression 类继承自 CompoundExpression，
+ * 用于实现减法运算的解释器模式。
+ */
 import { CompoundExpression } from '../InterpreterPattern'
 
 class SubtractionExpression extends CompoundExpression {
+    /**
+     * 执行减法运算并返回结果。
+     * @returns {number} 返回两个表达式相减的结果。
+     */
     interpret() {
+        // 调用两个子表达式的 interpret 方法并执行减法运算
         return this.expression1.interpret() - this.expression2.interpret()
     }
 }
