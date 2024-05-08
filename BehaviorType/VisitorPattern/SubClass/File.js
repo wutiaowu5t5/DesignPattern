f5a3e8092ea184587880e208455c345814aeb670
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 17:26:06 +0800

    docs：complete the subClass of visitor pattern

--- BehaviorType/VisitorPattern/SubClass/File.js
+++ BehaviorType/VisitorPattern/SubClass/File.js
@@ -6,7 +6,17 @@
 
 import { Visitor } from '../VisitorPattern'
 
+/**
+ * FileSystem类表示文件系统的一个抽象基类。
+ * 该类提供了基本的文件系统对象功能，包括接受访问者和获取文件类型的方法。
+ * 该类不应该直接实例化，应由其子类实例化。
+ */
 class FileSystem {
+    /**
+     * 构造函数初始化文件系统对象。
+     * @param {string} name 文件系统的名称。
+     * @throws {TypeError} 如果尝试直接构造FileSystem实例，则抛出TypeError。
+     */
     constructor(name) {
         if (new.target === FileSystem) {
             throw new TypeError('Cannot construct File instances directly')
@@ -14,7 +24,11 @@ class FileSystem {
         this.name = name
     }
     
-    // 接受访问者对象
+    /**
+     * 接受一个访问者对象并对当前对象进行访问。
+     * @param {Visitor} visitor 访问者对象。
+     * @throws {TypeError} 如果传入的参数不是Visitor类型，则抛出TypeError。
+     */
     accept(visitor) {
         if (!(visitor instanceof Visitor)) {
             throw new TypeError('Visitor object expected')
@@ -22,7 +36,11 @@ class FileSystem {
         visitor.visit(this)
     }
     
-    // 获取文件类型的方法
+    /**
+     * 获取文件类型的抽象方法。
+     * 该方法应由子类实现，以提供具体的文件类型信息。
+     * @throws {Error} 当调用该方法时，抛出一个错误，提示必须由子类实现该方法。
+     */
     getType() {
         throw new Error("getType method must be implemented by subclasses")
     }
