cdae3fb073e60b34e3d0fb1bb64aced3f404c158
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 16:49:56 +0800

    docs：complete the use state pattern

--- BehaviorType/StatePattern/UseStatePattern/UseStatePattern.js
+++ BehaviorType/StatePattern/UseStatePattern/UseStatePattern.js
@@ -4,18 +4,30 @@
  * @Time: 2024/5/7 18:21
  */
 
+/**
+ * 导入VendingMachine类，该类实现了状态模式
+ */
 import VendingMachine from '../StatePattern'
 
-const vendingMachine = new VendingMachine(10) // 初始化有10个商品
+/**
+ * 初始化一个拥有10个商品的自动售货机实例
+ */
+const vendingMachine = new VendingMachine(10)
 
-// 当前状态为有货状态
+/**
+ * 在有货状态下进行商品销售和补货操作的示例
+ */
 vendingMachine.sellProduct() // 输出：商品已售出
 vendingMachine.restock() // 输出：无法补货，售货机中已有商品
 
-// 切换到缺货状态
+/**
+ * 切换自动售货机到缺货状态，并进行操作示例
+ */
 vendingMachine.setState(vendingMachine.getSoldOutState())
 vendingMachine.sellProduct() // 输出：无法售出，商品已售罄
 vendingMachine.restock() // 输出：补货成功
 
-// 尝试传入无效状态
+/**
+ * 尝试传入无效状态进行测试
+ */
 vendingMachine.setState("InvalidState") // 输出：无效的状态对象
