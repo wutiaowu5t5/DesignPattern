06e71f4bb627441ef25aee4ccdbe31e8bbfb7687
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 14:57:50 +0800

    feat：complete the subclass of command pattern

--- BehaviorType/CommandPattern/SubClass/Television.js
+++ BehaviorType/CommandPattern/SubClass/Television.js
@@ -3,3 +3,15 @@
  * @Author: 5t5
  * @Time: 2024/5/8 14:47
  */
+
+class Television {
+    turnOn() {
+        console.log("电视已打开")
+    }
+    
+    turnOff() {
+        console.log("电视已关闭")
+    }
+}
+
+export default Television
