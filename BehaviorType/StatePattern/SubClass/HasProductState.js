da81c5642df1b64040c00db07d93f7ad8ec26997
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 16:49:42 +0800

    docs：complete the subClass of state pattern

--- BehaviorType/StatePattern/SubClass/HasProductState.js
+++ BehaviorType/StatePattern/SubClass/HasProductState.js
@@ -4,14 +4,25 @@
  * @Time: 2024/5/8 16:40
  */
 
+/**
+ * 该类表示售货机已有商品的状态。
+ * 继承自VendingMachineState，以提供特定于售出商品状态的行为。
+ */
 import VendingMachineState from './VendingMachineState'
 
 class HasProductState extends VendingMachineState {
+    /**
+     * 出售产品的方法。当售出产品时，将状态切换到售罄状态。
+     */
     sellProduct() {
         console.log("商品已售出")
         this.vendingMachine.setState(this.vendingMachine.getSoldOutState())
     }
     
+    /**
+     * 尝试为售货机补货的方法。在已有商品的状态下，该操作不被允许，
+     * 所以只是打印一条信息并不进行任何操作。
+     */
     restock() {
         console.log("无法补货，售货机中已有商品")
     }
