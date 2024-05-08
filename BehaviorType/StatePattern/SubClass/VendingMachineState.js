db71c0d8dae4ef208eab80b9272bb737e24400c2
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 16:46:09 +0800

    feat：complete the subClass of state pattern

--- BehaviorType/StatePattern/SubClass/VendingMachineState.js
+++ BehaviorType/StatePattern/SubClass/VendingMachineState.js
@@ -1,5 +1,21 @@
 /*
- * @Description: VendingMachineState.js
+ * @Description: 状态模式子类-售货机状态基类
  * @Author: 5t5
  * @Time: 2024/5/8 16:40
  */
+
+class VendingMachineState {
+    constructor(vendingMachine) {
+        this.vendingMachine = vendingMachine
+    }
+    
+    sellProduct() {
+        throw new Error('子类必须实现此方法')
+    }
+    
+    restock() {
+        throw new Error('子类必须实现此方法')
+    }
+}
+
+export default VendingMachineState
