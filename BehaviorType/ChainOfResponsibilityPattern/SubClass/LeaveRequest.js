b3b725457c2d3553a5d1a3167c851f394d8c7b01
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 11:28:50 +0800

    docs：complete the subclass of chain of responsibility pattern

--- BehaviorType/ChainOfResponsibilityPattern/SubClass/LeaveRequest.js
+++ BehaviorType/ChainOfResponsibilityPattern/SubClass/LeaveRequest.js
@@ -4,10 +4,14 @@
  * @Time: 2024/5/8 11:19
  */
 
+/**
+ * LeaveRequest 类用于创建一个请假申请实例。
+ * @param {number} leaveDays 请假天数。
+ */
 class LeaveRequest {
     constructor(leaveDays) {
-        this.leaveDays = leaveDays
+        this.leaveDays = leaveDays; // 初始化请假天数
     }
 }
 
-export default LeaveRequest
+export default LeaveRequest; // 导出LeaveRequest类作为默认模块
