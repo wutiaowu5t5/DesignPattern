a641de2d026bd072e5872bb94cafe6814eb0e482
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 14:57:47 +0800

    feat：complete the subclass of command pattern

--- BehaviorType/CommandPattern/SubClass/Stereo.js
+++ BehaviorType/CommandPattern/SubClass/Stereo.js
@@ -3,3 +3,15 @@
  * @Author: 5t5
  * @Time: 2024/5/8 14:47
  */
+
+class Stereo {
+    turnOn() {
+        console.log("音响已打开")
+    }
+    
+    turnOff() {
+        console.log("音响已关闭")
+    }
+}
+
+export default Stereo
