34f59a4805eabd1c60bdef1c1e61d0a240a1ccf2
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 11:24:25 +0800

    feat：complete the chain of responsibility pattern

--- BehaviorType/ChainOfResponsibilityPattern/ChainOfResponsibilityPattern.js
+++ BehaviorType/ChainOfResponsibilityPattern/ChainOfResponsibilityPattern.js
@@ -1,5 +1,29 @@
 /*
- * @Description: ChainOfResponsibilityPattern.js
+ * @Description: 行为型-职责链模式
  * @Author: 5t5
  * @Time: 2024/5/7 18:16
  */
+
+class Processor {
+    constructor(name, authorityLevel) {
+        if (new.target === Processor) {
+            throw new Error("不能实例化抽象类Processor")
+        }
+        this.name = name
+        this.authorityLevel = authorityLevel
+        this.nextProcessor = null
+    }
+    
+    setNextProcessor(nextProcessor) {
+        if (!(nextProcessor instanceof Processor)) {
+            throw new Error("下一个审批者必须是Processor的实例")
+        }
+        this.nextProcessor = nextProcessor
+    }
+    
+    processRequest(request) {
+        throw new Error("必须实现processRequest方法")
+    }
+}
+
+export default Processor
