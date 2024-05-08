02af3fb28b03076ebcbdd66ecd97f808c59a9f18
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 18:17:59 +0800

    feat：complete the subClass of interpreter pattern

--- BehaviorType/InterpreterPattern/SubClass/DivisionExpression.js
+++ BehaviorType/InterpreterPattern/SubClass/DivisionExpression.js
@@ -1,5 +1,19 @@
 /*
- * @Description: DivisionExpression.js
+ * @Description: 表达式子类-除法
  * @Author: 5t5
  * @Time: 2024/5/8 18:11
  */
+
+import { CompoundExpression } from '../InterpreterPattern'
+
+class DivisionExpression extends CompoundExpression {
+    interpret() {
+        const divisor = this.expression2.interpret()
+        if (divisor === 0) {
+            throw new Error("Division by zero")
+        }
+        return this.expression1.interpret() / divisor
+    }
+}
+
+export default DivisionExpression
