002a9149772534614872070f70a12f844f7b6d68
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 18:18:03 +0800

    feat：complete the subClass of interpreter pattern

--- BehaviorType/InterpreterPattern/SubClass/ModuloExpression.js
+++ BehaviorType/InterpreterPattern/SubClass/ModuloExpression.js
@@ -1,5 +1,19 @@
 /*
- * @Description: ModuloExpression.js
+ * @Description: 表达式子类-取余
  * @Author: 5t5
  * @Time: 2024/5/8 18:11
  */
+
+import { CompoundExpression } from '../InterpreterPattern'
+
+class ModuloExpression extends CompoundExpression {
+    interpret() {
+        const divisor = this.expression2.interpret()
+        if (divisor === 0) {
+            throw new Error("Modulo by zero")
+        }
+        return this.expression1.interpret() % divisor
+    }
+}
+
+export default ModuloExpression
