7e949f804f609512982cc9c2846af93312c0d9c4
wutiaowu5t5 <1987053649@qq.com>
2024-05-08 11:42:43 +0800

    style：modify file style

--- StructuralType/ProxyPattern/ByClass/UseProxyPattern/UseProxyPattern.js
+++ StructuralType/ProxyPattern/ByClass/UseProxyPattern/UseProxyPattern.js
@@ -12,14 +12,14 @@ const target = {
     name: "Alice",
     age: 25,
     greet() {
-        console.log(`Hello, my name is ${this.name}`);
+        console.log(`Hello, my name is ${this.name}`)
     }
-};
+}
 
 // 使用代理类来创建代理对象
-const handler = new ProxyHandler(target);
+const handler = new ProxyHandler(target)
 
 // 使用代理对象
-console.log(handler.get('name')); // 获取 name 属性，拦截器将打印日志
-handler.set('age', 30); // 设置 age 属性，拦截器将打印日志
-handler.apply('greet'); // 调用 greet 方法，拦截器将打印日志
+console.log(handler.get('name')) // 获取 name 属性，拦截器将打印日志
+handler.set('age', 30) // 设置 age 属性，拦截器将打印日志
+handler.apply('greet') // 调用 greet 方法，拦截器将打印日志
